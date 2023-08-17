848ce68c0d5f9e9a69e414c1fed51975eaf6a767
Angelique15 <113044854+Angelique15@users.noreply.github.com>
2023-08-17 20:21:44 +0000

    Deploying to gh-pages from @ Angelique15/goit-react-hw-08-phonebook@fd4a6c986e6113a7a1d9f488a62539d42b9e4958 🚀

--- src/redux/auth/authSelectors.js
+++
@@ -1,4 +0,0 @@
-// redux/auth/authSelectors.js
-export const getIsLoggedIn = (state) => state.auth.user !== null;
-export const getUser = (state) => state.auth.user;
-
--- src/redux/contacts/contactsSelectors.js
+++
@@ -1,8 +0,0 @@
-// Get all contacts
-export const selectAllContacts = (state) => state.contacts.contacts;
-
-// Get loading state
-export const selectLoading = (state) => state.contacts.isLoading;
-
-// Get error state
-export const selectError = (state) => state.contacts.error;
